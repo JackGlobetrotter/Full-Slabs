c45092a3f9425a149ff781441ca4cb48e2a45253
Jakob Dickert <jakob.dickert@outlook.com>
2023-02-16 17:34:56 +0100

    Changed to JDM

---
+++ src/main/java/dev/jdm/full_slabs/client/render/model/ExtraSlabModel.java
@@ -0,0 +1,53 @@
+package dev.jdm.full_slabs.client.render.model;
+
+import dev.jdm.full_slabs.block.ExtraSlabBlock;
+import dev.jdm.full_slabs.block.SlabBlockUtility;
+import dev.jdm.full_slabs.block.entity.ExtraSlabBlockEntity;
+import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.model.UnbakedModel;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.joml.Vector3f;
+import net.minecraft.world.BlockRenderView;
+
+import net.minecraft.util.math.random.Random;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class ExtraSlabModel extends BasicModel {
+	
+	@Override
+	public void emitBlockQuads(BlockRenderView view, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
+		ExtraSlabBlockEntity entity = (ExtraSlabBlockEntity) view.getBlockEntity(pos);
+		if(entity == null) return;
+		Direction direction = SlabBlockUtility.getDirection(state.get(ExtraSlabBlock.TYPE), state.get(ExtraSlabBlock.AXIS));
+		BlockState baseState = entity.getBaseState();
+		BlockState extraState = entity.getExtraState();
+		emitModel(view, baseState, pos, randomSupplier, context);
+		if(extraState != null) {
+			// This section just translates the extra model half a block
+			Vector3f unit = direction.getOpposite().getUnitVector();
+			unit.normalize(0.5f); // Half normal of inner slab face
+			context.pushTransform((quad) -> {
+				for(int i = 0; i < 4; ++i) {
+					Vector3f vec = quad.copyPos(i, null);
+					vec.add(unit);
+					quad.pos(i, vec);
+				}
+				return true;
+			});
+			emitModel(view, extraState, pos, randomSupplier, context);
+			context.popTransform();
+		}
+	}
+
+	@Override
+	public void setParents(Function<Identifier, UnbakedModel> modelLoader) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
