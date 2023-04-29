40cd64af38436a9da0301653cda327fa5432711f
JackGlobetrotter <jakob.dickert@outlook.com>
2023-04-29 14:00:54 +0200

    Fail if Sodium is loaded w/o Indium

--- src/main/java/dev/jdm/full_slabs/FullSlabsModClient.java
+++ src/main/java/dev/jdm/full_slabs/FullSlabsModClient.java
@@ -3,6 +3,7 @@ package dev.jdm.full_slabs;
 import dev.jdm.full_slabs.client.render.OutlineRenderer;
 import dev.jdm.full_slabs.client.render.OverlayRenderer;
 import dev.jdm.full_slabs.client.render.model.FullSlabModelProvider;
+import dev.jdm.full_slabs.compatibility.SodiumCompatibility;
 import dev.jdm.full_slabs.config.CustomControls;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
@@ -16,5 +17,7 @@ public class FullSlabsModClient implements ClientModInitializer {
 		if(FabricLoader.getInstance().isModLoaded("malilib"))
 			OverlayRenderer.init();
 		CustomControls.clientInit();
+
+		SodiumCompatibility.load();
 	}
 }
---
+++ src/main/java/dev/jdm/full_slabs/compatibility/SodiumCompatibility.java
@@ -0,0 +1,19 @@
+package dev.jdm.full_slabs.compatibility;
+
+import dev.jdm.full_slabs.FullSlabsMod;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class SodiumCompatibility {
+
+    public static void load(){
+        FabricLoader loader = FabricLoader.getInstance();
+        if (loader.isModLoaded("sodium") && !loader.isModLoaded("indium")) {
+            String issue = "Fullslabs mod requires Indium mod to be loaded if Sodium mod is used";
+            String action = "Include Indium mod to make Fullslabs mod work with sodium";
+
+            FullSlabsMod.LOGGER.error(issue);
+            FullSlabsMod.LOGGER.error(action);
+            throw new RuntimeException(issue + ". " + action);
+        }
+    }
+}
